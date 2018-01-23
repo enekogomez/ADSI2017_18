17c6f37f2038bb5c8dd567c9e390a5efc2e3a749
enekogomez <enekogomez@users.noreply.github.com>
2018-01-23 19:13:05 +0100

    Create GestorRSGoogle.java

---
+++ src/packConexion/GestorRSGoogle.java
@@ -0,0 +1,33 @@
+public class GestorRSGoogle implements AuthInterface{
+
+
+    private GestorRSGoogle unGestorRSGoogle;
+    private static GestorGoogle gestorGoogle = null;
+
+    public GestorRSGoogle(){
+
+    }
+
+    public GestorRSGoogle getGestorRSFacebook(){
+        if(unGestorRSGoogle == null){
+            return new GestorRSGoogle();
+        }else return this;
+    }
+
+    @Override
+    public boolean login() {
+        gestorGoogle = GestorGoogle.init();
+        return GestorGoogle.login();
+    }
+
+    @Override
+    public boolean post(String message, String token) {
+        return gestorGoogle.publish(message, token);
+    }
+
+    @Override
+    public void close() {
+        GestorGoogle.disconnect();
+        gestorGoogle = null;
+    }
+}
